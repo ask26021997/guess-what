cda64b8ba22193c52e176f804f10b1dfc2f8423a
Anand Singh <ask26021997@gmail.com>
2022-08-29 13:01:17 +0530

    port issue changes - 001

--- src/app.js
+++ src/app.js
@@ -15,8 +15,6 @@ app.use(expressLayouts);
 app.use(express.static("src/public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const PORT = process.env.GUESST_IT_PORT || 5000;
-
 const mongoose = require("mongoose");
 
 const db = mongoose.connection;
@@ -33,8 +31,9 @@ mongoose.connect(process.env.DATABASE_URL, {
 
 require("./routers")(app);
 
-
-app.listen(PORT, ()=>{
-  console.log(`Guess IT game server running at: http://localhost:${PORT}`)
-})
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log("Press Ctrl+C to quit.");
+});
 
