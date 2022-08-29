b131cebf2a58656357dbf87819b301a51c598776
Anand Singh <ask26021997@gmail.com>
2022-08-29 13:06:58 +0530

    port issue changes - 002

--- src/routers/index.js
+++ src/routers/index.js
@@ -7,8 +7,8 @@ const registerRoutes = app => {
   router.get("/", (req, res)=>{
     res.render("index");
   })
-  app.use("/", router);
-  const gameRouter = require("./game.routers")(app)
+  app.get("/", router);
+  require("./game.routers")(app);
 }
 
 module.exports=registerRoutes;
